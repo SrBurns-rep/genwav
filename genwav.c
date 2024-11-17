3ff1e26f7d69c6a17e68ef4d9dd43a8db223851c
Caio Burns Lessa <caiobl.25@gmail.com>
2024-11-17 01:13:06 -0300

    added wav_alloc_mono and wav_write

--- genwav.c
+++ genwav.c
@@ -2,7 +2,6 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
-#include <varargs.h>
 
 // Defaults
 #define SAMPLE_RATE		44100
@@ -52,7 +51,24 @@ void sin_tone(WavFile *file, float amp, float base_freq) {
 	}
 }
 
-WavFile *alloc_file_mono(size_t sample_size) {
+int write_wav(WavFile *file_buff, const char *name) {
+	FILE *file = fopen(name, "w");
+	if(file == NULL){
+		perror("fopen");
+		return -1;
+	}
+	
+	size_t bytes_wrote = fwrite(file_buff, 1, file_buff->header.format.file_size, file);
+	if(bytes_wrote == 0){
+		perror("fwrite");
+		return -1;
+	}
+
+	fclose(file);
+	return 0;
+}
+
+WavFile *wav_alloc_mono(size_t sample_size) {
 
 	const size_t num_channels = 1;
 
@@ -85,56 +101,17 @@ WavFile *alloc_file_mono(size_t sample_size) {
 
 int main(void) {
 
-	//const size_t data_size = SAMPLE_RATE * DURATION_SECS * 2;
-	const size_t data_size = 101 * 2;
-	const size_t total_size = sizeof(WavFile) + data_size;
-
-	WavFile *file_buff = malloc(total_size);
-
-	(*file_buff) = (WavFile){
-		.header.format = {
-			.RIFF = {'R', 'I', 'F', 'F'},
-			.file_size = total_size,
-			.WAVE = {'W', 'A', 'V', 'E'},
-			.fmt = {'f', 'm', 't', ' '},
-			.fmt_length = 16,
-			.fmt_type = 1,
-			.num_channels = NUM_CHANNELS,
-			.sample_rate = SAMPLE_RATE,
-			.byte_rate = (SAMPLE_RATE * BITS_PER_SAMPLE * NUM_CHANNELS) / 8,
-			.block_align = (BITS_PER_SAMPLE * NUM_CHANNELS),
-			.bits_per_sample = BITS_PER_SAMPLE,
-			.DATA = {'d', 'a', 't', 'a'},
-			.data_size = data_size
-		}
-	};
-
-	const float sample_time = 1.f / SAMPLE_RATE;
-	float dt = 0;
-	const float base_freq = 440.f;
-	size_t duration = 101;
-	
-	for(size_t i = 0; i < duration; i++) {
-		file_buff->data[i] = sin_tone(dt, powf(2.f, 15.f) / 2.f, base_freq);
-
-		dt += sample_time;
-		dt = fmodf(dt, 1);
-	}
-
-	FILE *file = fopen("test.wav", "w");
-	if(file == NULL){
-		perror("fopen");
-		return -1;
-	}
-	
-	size_t bytes_wrote = fwrite(file_buff, 1, total_size, file);
-	if(bytes_wrote == 0){
-		perror("fwrite");
+	WavFile *file_buff = wav_alloc_mono(SAMPLE_RATE * DURATION_SECS);
+	if(file_buff == NULL) {
+		perror("wav_alloc_mono");
 		return -1;
 	}
 
-	fclose(file);
+	sin_tone(file_buff, pow(2, 15) / 2.f, 440.f);
+
+	int err = write_wav(file_buff, "test0.wav");
+
 	free(file_buff);
 
-	return 0;
+	return err;
 }
