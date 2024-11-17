afb87679208e7bb0bc5858cc6dbb43ca596ebfb8
Caio Burns Lessa <caiobl.25@gmail.com>
2024-11-17 00:55:13 -0300

    antes da função sin_tone nova

--- genwav.c
+++ genwav.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
+#include <varargs.h>
 
 // Defaults
 #define SAMPLE_RATE		44100
@@ -36,18 +37,56 @@ typedef struct WavFile {
 	int16_t data[];
 }WavFile;
 
-int16_t sin_tone(float dt, float amp, float base_freq) {
+void sin_tone(WavFile *file, float amp, float base_freq) {
 
-	int16_t y = (int16_t)( amp * sinf(base_freq * dt * M_PI));
+	const float sample_time = 1.f / SAMPLE_RATE;
+	float dt = 0;
 
-	return y;
+
+	for(size_t i = 0; i < file->header.format.data_size / 2; i++) {
+		
+		file->data[i] = (int16_t)( amp * sinf(base_freq * dt * 2 * M_PI));
+
+		dt += sample_time;
+		dt = fmodf(dt, 1);
+	}
 }
 
-void apply_tone(uint16_t);
+WavFile *alloc_file_mono(size_t sample_size) {
+
+	const size_t num_channels = 1;
+
+	size_t data_size = sample_size * sizeof(int16_t);
+	size_t total_size = data_size + sizeof(WavFile);
+
+	WavFile *file_buff = malloc(total_size);
+
+	(*file_buff) = (WavFile){
+		.header.format = {
+			.RIFF = {'R', 'I', 'F', 'F'},
+			.file_size = total_size,
+			.WAVE = {'W', 'A', 'V', 'E'},
+			.fmt = {'f', 'm', 't', ' '},
+			.fmt_length = 16,
+			.fmt_type = 1,
+			.num_channels = num_channels,
+			.sample_rate = SAMPLE_RATE,
+			.byte_rate = (SAMPLE_RATE * BITS_PER_SAMPLE * num_channels) / 8,
+			.block_align = (BITS_PER_SAMPLE * num_channels),
+			.bits_per_sample = BITS_PER_SAMPLE,
+			.DATA = {'d', 'a', 't', 'a'},
+			.data_size = data_size
+		}
+	};
+
+
+	return file_buff;
+}
 
 int main(void) {
 
-	const size_t data_size = SAMPLE_RATE * DURATION_SECS * 2;
+	//const size_t data_size = SAMPLE_RATE * DURATION_SECS * 2;
+	const size_t data_size = 101 * 2;
 	const size_t total_size = sizeof(WavFile) + data_size;
 
 	WavFile *file_buff = malloc(total_size);
@@ -72,9 +111,11 @@ int main(void) {
 
 	const float sample_time = 1.f / SAMPLE_RATE;
 	float dt = 0;
-
-	for(size_t i = 0; i < (data_size / 2); i++) {	
-		file_buff->data[i] = sin_tone(dt, powf(2.f, 15.f) / 2.f, 440.f);
+	const float base_freq = 440.f;
+	size_t duration = 101;
+	
+	for(size_t i = 0; i < duration; i++) {
+		file_buff->data[i] = sin_tone(dt, powf(2.f, 15.f) / 2.f, base_freq);
 
 		dt += sample_time;
 		dt = fmodf(dt, 1);
